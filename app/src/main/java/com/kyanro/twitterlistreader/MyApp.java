85dabdb81f8af8f1c578ee4221459bad812b54f3
kyanro <gw.fnct+github@gmail.com>
2015-02-01 22:46:32 +0900

    起動時に fabric の初期化をするために MyAppクラスを作成
    new Twitter(authConfig) だと twitter kit 全部？

---
+++ app/src/main/java/com/kyanro/twitterlistreader/MyApp.java
@@ -0,0 +1,21 @@
+package com.kyanro.twitterlistreader;
+
+import android.app.Application;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MyApp extends Application {
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = BuildConfig.TWITTER_KEY;
+    private static final String TWITTER_SECRET = BuildConfig.TWITTER_SECRET;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+    }
+}
